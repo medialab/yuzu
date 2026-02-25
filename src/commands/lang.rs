1b5a2e9e921c7d60de9b16f30c18319ceddbfb77
Yomguithereal <guillaumeplique@gmail.com>
2026-02-25 16:54:58 +0100

    Test POC

--- src/commands/lang.rs
+++ src/commands/lang.rs
@@ -1,7 +1,7 @@
 use clap::Args;
 use simd_csv::ByteRecord;
 
-use crate::utils::io::CSVInput;
+use crate::utils::io::{CSVInput, CSVOutput};
 use crate::utils::select::SelectedColumns;
 use crate::{CLIResult, CommonArgs};
 
@@ -11,6 +11,9 @@ pub struct LangArgs {
     column: SelectedColumns,
     /// Path to CSV file containing text to classify (will use stdin if not given or if path is "-").
     input: Option<String>,
+    /// Path to output file. Will write to stdout if not given or if path is "-".
+    #[arg(short, long)]
+    output: Option<String>,
     #[command(flatten)]
     common: CommonArgs,
 }
@@ -19,15 +22,21 @@ pub fn action(args: LangArgs) -> CLIResult<()> {
     let mut reader = CSVInput::new(&args.input)
         .delimiter(args.common.delimiter)
         .csv_reader()?;
-    let headers = reader.byte_headers()?;
-    let column_index = args.column.single_selection(headers, true)?;
+    let mut headers = reader.byte_headers()?.clone();
+    let column_index = args.column.single_selection(&headers, true)?;
+
+    let mut writer = CSVOutput::new(&args.output).csv_writer()?;
+
+    headers.push_field(b"lang");
+
+    writer.write_byte_record(&headers)?;
 
     let mut record = ByteRecord::new();
 
     while reader.read_byte_record(&mut record)? {
-        let text = &record[column_index];
-
-        dbg!("{}", text);
+        let _text = &record[column_index];
+        record.push_field(b"en");
+        writer.write_byte_record(&record)?;
     }
 
     Ok(())
---
+++ tests/test_lang.rs
@@ -0,0 +1,13 @@
+use crate::cmd;
+
+#[test]
+fn lang() {
+    cmd()
+        .arg("lang")
+        .arg("sentence")
+        .write_csv_stdin(&[&["sentence"], &["this is an English sentence"]])
+        .assert_csv(&[
+            &["sentence", "lang"],
+            &["this is an English sentence", "en"],
+        ]);
+}
